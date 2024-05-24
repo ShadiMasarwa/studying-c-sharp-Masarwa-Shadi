676e46f7b58a5a1bbd4c7a39b8ac7aceb6b37487
ShadiMasarwa <96420608+ShadiMasarwa@users.noreply.github.com>
2024-05-24 09:11:19 +0300

    public-internal

--- studying-c-sharp-Masarwa-Shadi/Program.cs
+++ studying-c-sharp-Masarwa-Shadi/Program.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace studying_c_sharp_Masarwa_Shadi
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
