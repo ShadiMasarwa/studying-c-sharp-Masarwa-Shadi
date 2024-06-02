e9eca19d01e037b7bf8566c020ee8ab47840509a
visual <visual@hermelin.ort.org.il>
2024-06-02 21:00:06 +0300

    Data Structure Q1

--- studying-c-sharp-Masarwa-Shadi/Program.cs
+++ studying-c-sharp-Masarwa-Shadi/Program.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using studying_c_sharp_Masarwa_Shadi.demo;
+using studying_c_sharp_Masarwa_Shadi.T1;
+using studying_c_sharp_Masarwa_Shadi.DS;
 
 namespace studying_c_sharp_Masarwa_Shadi
 {
@@ -11,8 +13,20 @@ namespace studying_c_sharp_Masarwa_Shadi
     {
         public static void Main(string[] args)
         {
-            Level1.RunMe();
-            Console.WriteLine("aaa");
+            //Level1.RunMe();
+            //Console.WriteLine("aaa");
+            Node<int> n1 = new Node<int>(10);
+            Node<int> n2 = new Node<int>(15);
+            Node<int> n3 = new Node<int>(17);
+            Node<int> n4 = new Node<int>(16);
+            Node<int> n5 = new Node<int>(48);
+            n1.SetNext(n2);
+            n2.SetNext(n3);
+            n3.SetNext(n4);
+            n4.SetNext(n5);
+            
+            Node<int> result = Q1.Even(n1);
+            Console.WriteLine(result.ToPrint());
         }
     }
 }
---
+++ studying-c-sharp-Masarwa-Shadi/T1/Q1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using studying_c_sharp_Masarwa_Shadi.DS;
+
+namespace studying_c_sharp_Masarwa_Shadi.T1
+{
+    class Q1
+    {
+        public static Node<int> Even(Node<int> lst)
+        {
+            Node<int> pos = lst;
+            Node<int> newlst = null;
+            while(pos != null)
+            {
+                if(pos.GetValue() % 2 == 0)
+                {
+                    if (newlst==null)
+                        newlst = new Node<int>(pos.GetValue());
+                    else
+                        newlst = new Node<int>(pos.GetValue(), newlst);
+                }
+                pos = pos.GetNext();
+                
+            }
+            return newlst;
+        } 
+    }
+}
