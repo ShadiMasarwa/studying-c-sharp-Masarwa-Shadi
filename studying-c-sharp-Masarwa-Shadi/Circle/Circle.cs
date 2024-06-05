fad6b8d1934ca8c43ce196d3809ce70d573aa894
ShadiMasarwa <masarwa@gmail.com>
2024-06-05 14:20:18 +0300

    Circle1 & Circle2 Classes

---
+++ studying-c-sharp-Masarwa-Shadi/Circle/Circle.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace studying_c_sharp_Masarwa_Shadi.Circle
+{
+    public class TestCircle1
+    {  
+        public static void RunMe()
+        {
+            Circle c1 = new Circle();
+            Console.WriteLine(c1.toString());
+            Console.WriteLine("The circle has radius of " + c1.getRadius() + " and area of " + c1.getArea());
+            c1.setColor("black");
+            Console.WriteLine(c1.toString());
+            Console.WriteLine();
+
+            Circle c2 = new Circle(2.0);
+            Console.WriteLine(c2.toString());
+            Console.WriteLine ("The circle has radius of " + c2.getRadius() + " and area of " + c2.getArea());
+            c2.setRadius(3.8);
+            Console.WriteLine("The circle has radius of " + c2.getRadius() + " and area of " + c2.getArea());
+        }
+    }
+    public class Circle
+    {
+        private double radius;
+        private string color;
+
+        public Circle()
+        {  
+            radius = 1.0;
+            color = "red";
+        }
+
+        public Circle(double r)
+        {  
+            radius = r;
+            color = "red";
+        }
+
+        public double getRadius()
+        {
+            return radius;
+        }
+
+        public void setRadius(double radius)
+        {
+            this.radius = radius;
+        }
+
+        public string getColor()
+        {
+            return color;
+        }
+
+        public void setColor(string color)
+        {
+            this.color = color;
+        }
+
+        public double getArea()
+        {
+            return radius * radius * Math.PI;
+        }
+
+        public string toString()
+        {
+            return "Circle [radius=" + radius + " color=" + color + "]";
+        }
+    }
+}
---
+++ studying-c-sharp-Masarwa-Shadi/Circle2/Circle2.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace studying_c_sharp_Masarwa_Shadi.Circle2
+{
+    public class TestCircle2
+    {
+        public static void RunMe()
+        {
+            Circle2 c1 = new Circle2(1.1);
+            Console.WriteLine("Circle 1A:");
+            Console.WriteLine(c1.toString());
+            Console.WriteLine("radius is: " + c1.getRadius());
+            Console.WriteLine("area is: " + c1.getArea().ToString("0.00"));
+            Console.WriteLine("circumference is: " + c1.getCircumference().ToString("0.00"));
+            Console.WriteLine();
+            c1.setRadius(2.2);
+            Console.WriteLine("Circle 1B:");
+            Console.WriteLine(c1.toString());
+            Console.WriteLine("radius is: " + c1.getRadius());
+            Console.WriteLine("area is: " + c1.getArea().ToString("0.00"));
+            Console.WriteLine("circumference is: " + c1.getCircumference().ToString("0.00"));
+            Console.WriteLine();
+            Circle2 c2 = new Circle2();
+            Console.WriteLine("Circle 2A:");
+            Console.WriteLine(c2.toString());
+            Console.WriteLine("radius is: " + c2.getRadius());
+            Console.WriteLine("area is: " + c2.getArea().ToString("0.00"));
+            Console.WriteLine("circumference is: " + c2.getCircumference().ToString("0.00"));
+            Console.WriteLine();
+            c2.setRadius(5.6);
+            Console.WriteLine("Circle 2B:");
+            Console.WriteLine(c2.toString());
+            Console.WriteLine("radius is: " + c2.getRadius());
+            Console.WriteLine("area is: " + c2.getArea().ToString("0.00"));
+            Console.WriteLine("circumference is: " + c2.getCircumference().ToString("0.00"));
+
+
+        }
+    }
+
+    
+        public class Circle2
+        {
+            private double radius;
+
+            public Circle2()
+            {
+                radius = 1.0;
+            }
+
+            public Circle2(double radius)
+            {
+                this.radius = radius;
+            }
+
+            public double getRadius()
+            {
+                return radius;
+            }
+
+            public void setRadius(double radius)
+            {
+                this.radius = radius;
+            }
+
+
+            public double getArea()
+            {
+                return Math.Pow(radius,2) * Math.PI;
+            }
+
+            public double getCircumference()
+            {
+                return  2 * radius * Math.PI;
+            }
+
+            public string toString()
+            {
+                return "Circle [radius=" + radius + "]";
+            }
+        }
+}
