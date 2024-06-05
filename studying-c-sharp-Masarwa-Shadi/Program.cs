8a2711b663c3762b4c9f16ef208fc37bcb316c31
ShadiMasarwa <masarwa@gmail.com>
2024-06-05 12:46:14 +0300

    Ds Changes

--- studying-c-sharp-Masarwa-Shadi/Program.cs
+++ studying-c-sharp-Masarwa-Shadi/Program.cs
@@ -5,7 +5,6 @@ using System.Text;
 using System.Threading.Tasks;
 using studying_c_sharp_Masarwa_Shadi.demo;
 using studying_c_sharp_Masarwa_Shadi.T1;
-using studying_c_sharp_Masarwa_Shadi.DS;
 
 namespace studying_c_sharp_Masarwa_Shadi
 {
@@ -14,19 +13,7 @@ namespace studying_c_sharp_Masarwa_Shadi
         public static void Main(string[] args)
         {
             //Level1.RunMe();
-            //Console.WriteLine("aaa");
-            Node<int> n1 = new Node<int>(10);
-            Node<int> n2 = new Node<int>(15);
-            Node<int> n3 = new Node<int>(17);
-            Node<int> n4 = new Node<int>(16);
-            Node<int> n5 = new Node<int>(48);
-            n1.SetNext(n2);
-            n2.SetNext(n3);
-            n3.SetNext(n4);
-            n4.SetNext(n5);
-            
-            Node<int> result = Q1.Even(n1);
-            Console.WriteLine(result.ToPrint());
+            Q1.RunMe();
         }
     }
 }
--- studying-c-sharp-Masarwa-Shadi/T1/Q1.cs
+++ studying-c-sharp-Masarwa-Shadi/T1/Q1.cs
@@ -9,6 +9,22 @@ namespace studying_c_sharp_Masarwa_Shadi.T1
 {
     class Q1
     {
+        public static void RunMe()
+        {
+            //Console.WriteLine("aaa");
+            Node<int> n1 = new Node<int>(10);
+            Node<int> n2 = new Node<int>(15);
+            Node<int> n3 = new Node<int>(17);
+            Node<int> n4 = new Node<int>(16);
+            Node<int> n5 = new Node<int>(48);
+            n1.SetNext(n2);
+            n2.SetNext(n3);
+            n3.SetNext(n4);
+            n4.SetNext(n5);
+
+            Node<int> result = Q1.Even(n1);
+            Console.WriteLine(result.ToPrint());
+        }
         public static Node<int> Even(Node<int> lst)
         {
             Node<int> pos = lst;
