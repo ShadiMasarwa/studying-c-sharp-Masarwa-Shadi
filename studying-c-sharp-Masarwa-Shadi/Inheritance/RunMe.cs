599047cbfbc942d650a1b5d18c2a9a64a4d09559
visual <visual@hermelin.ort.org.il>
2024-06-21 14:48:25 +0300

    Electric Products 1

---
+++ studying-c-sharp-Masarwa-Shadi/Inheritance/RunMe.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace studying_c_sharp_Masarwa_Shadi.Inheritance
+{
+    public class RunMe
+    {
+        public static void Run()
+        {
+            PlayStation p1 = new PlayStation(4);
+            p1.PrintDetails();
+
+            TV tv1 = new TV("Xi098m", false, 1987, true, "android");
+            tv1.PrintScreen();
+        }
+        
+    }
+}
