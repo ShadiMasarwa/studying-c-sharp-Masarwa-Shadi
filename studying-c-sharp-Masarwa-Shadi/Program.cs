c2875e7c7971fd1bc661b560c924c3d25636a812
visual <visual@hermelin.ort.org.il>
2024-06-23 22:28:39 +0300

    DS wether app

--- studying-c-sharp-Masarwa-Shadi/Program.cs
+++ studying-c-sharp-Masarwa-Shadi/Program.cs
@@ -6,10 +6,10 @@ using studying_c_sharp_Masarwa_Shadi.Circle;
 using studying_c_sharp_Masarwa_Shadi.Circle2;
 //Data Structure
 //using studying_c_sharp_Masarwa_Shadi.DS.T1;
-using studying_c_sharp_Masarwa_Shadi.DS.T2;
+//using studying_c_sharp_Masarwa_Shadi.DS.T2;
 
 using studying_c_sharp_Masarwa_Shadi.DS.ClassProj;
-using studying_c_sharp_Masarwa_Shadi.Inheritance;
+//using studying_c_sharp_Masarwa_Shadi.Inheritance;
 
 namespace studying_c_sharp_Masarwa_Shadi
 {
@@ -28,10 +28,11 @@ namespace studying_c_sharp_Masarwa_Shadi
             //a2.RunMe();
             //LinkedListExample.RunTasks();
             //RunMe.Run();
-            Q1.Run();
+            //Q1.Run();
             //Q2.RunMe();
             //Q3.RunMe();
             //Q4.RunMe();
+            Wether.RunMe();
         }
     }
 }
