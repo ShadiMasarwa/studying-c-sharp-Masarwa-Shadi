98378d50896929b395cdfd5609093d6e24c16bf4
ShadiMasarwa <masarwa@gmail.com>
2024-08-29 15:42:28 +0300

    Queue+BinNode implimintation

---
+++ studying-c-sharp-Masarwa-Shadi/DS/BinNode.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace studying_c_sharp_Masarwa_Shadi.DS
+{
+    public class BinNode<T>
+    {
+        private T value;
+        private BinNode<T> left;
+        private BinNode<T> right;
+
+        //-----------------------------------
+        // constructors
+        public BinNode(T value)
+        {
+            this.value = value;
+            this.left = null;
+            this.right = null;
+        }
+        public BinNode(BinNode<T> left, T value, BinNode<T> right)
+        {
+            this.value = value;
+            this.left = left;
+            this.right = right;
+        }
+        //-----------------------------------
+        // Getters
+        public T GetValue()
+        {
+            return value;
+        }
+
+        public BinNode<T> GetLeft()
+        {
+            return this.left;
+        }
+        public BinNode<T> GetRight()
+        {
+            return this.right;
+        }
+        //-----------------------------------
+        // Setters
+        public void SetValue(T value)
+        {
+            this.value = value;
+        }
+        public void SetLeft(BinNode<T> left)
+        {
+            this.left = left;
+        }
+
+        public void SetRight(BinNode<T> right)
+        {
+            this.right = right;
+        }
+        //-----------------------------------
+        // returns true if this.left is not null, else returns false
+        public bool HasLeft()
+        {
+            return (this.left != null);
+        }
+        // returns true if this.right is not null, else returns false
+        public bool HasRight()
+        {
+            return (this.right != null);
+        }
+        //-----------------------------------
+        //ToString
+        public override string ToString()
+        {
+            return "(" + left + " " + value + " " + right + ")";
+        }
+    }
+}
---
+++ studying-c-sharp-Masarwa-Shadi/DS/Queue.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace studying_c_sharp_Masarwa_Shadi.DS
+{
+    public class Queue<T>
+    {
+        private Node<T> first;
+        private Node<T> last;
+        public Queue()
+        {
+            first = null;
+            last = null;
+        }
+        public void Insert(T x)
+        {
+            if (last == null)
+            {
+                first = new Node<T>(x);
+                last = first;
+            }
+            else
+            {
+                last.SetNext(new Node<T>(x));
+                last = last.GetNext();
+            }
+        }
+        public T Remove()
+        {
+            T t = first.GetValue();
+            if (first != last)
+                first = first.GetNext();
+            else
+            {
+                first = null;
+                last = null;
+            }
+            return t;
+        }
+        public T Head() { return first.GetValue(); }
+        public bool IsEmpty() { return first == null; }
+        public override string ToString()
+        {
+            Node<T> temp = first;
+            string st = "[";
+            while (temp != null)
+            {
+                st += temp.GetValue() + ",";
+                temp = temp.GetNext();
+            }
+            st += "]";
+            return st;
+        }
+    }
+}
