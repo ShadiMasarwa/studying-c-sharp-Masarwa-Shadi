f0445084ba6581dc2f1a2ebce47be0103e059543
Administrator <Administrator@4115-003>
2024-09-22 20:35:10 +0300

    T4 DS final

---
+++ studying-c-sharp-Masarwa-Shadi/DS/S3/T4/Q3.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace studying_c_sharp_Masarwa_Shadi.DS.S3.T4
+{
+    public class Q3
+    {
+        public static Queue<int> Devided(Queue<int> q)
+        {
+            Queue<int> newQ = new Queue<int>();
+            Queue<int> temp = new Queue<int>();
+            int max = q.Remove();
+            while(!q.IsEmpty())
+            {
+                int num = q.Remove();
+                temp.Insert(num);
+                if (num != -1)
+                {
+                    if (num>max)
+                        max = num;
+                }
+                else
+                {
+                    newQ.Insert(max);
+                    max = q.Head();
+                }
+            }
+            q.Insert(temp.Remove());
+            return newQ;
+        }
+    }
+}
---
+++ studying-c-sharp-Masarwa-Shadi/DS/S3/T4/Q4.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace studying_c_sharp_Masarwa_Shadi.DS.S3.T4
+{
+    public class Q4
+    {
+        public static Queue<int> MultiplyQ(Queue<int> q)
+        {
+            Queue<int> result = new Queue<int>();
+            Queue<int> temp = new Queue<int>();
+            while(!q.IsEmpty())
+            {
+                int num = q.Remove();
+                temp.Insert(num);
+                for (int i = 0; i < num; i++)
+                    result.Insert(num);
+            }
+            while(!temp.IsEmpty())
+                q.Insert(temp.Remove());
+
+            return result;
+        }
+    }
+}
