057c6725bd5fd6ee00871068394dd40af6374ad6
Administrator <Administrator@4115-003>
2024-09-22 20:05:43 +0300

    Queue Targil 4

---
+++ studying-c-sharp-Masarwa-Shadi/DS/S3/T4/Q1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace studying_c_sharp_Masarwa_Shadi.DS.S3.T4
+{
+    public class Q1
+    {
+        private static int CountNum(Queue<int> q, int num)
+        {
+            int count = 0;
+            Queue<int> temp = new Queue<int>();
+            while (!q.IsEmpty())
+            {
+                int n = q.Remove();
+                if (n==num)
+                    count++;
+                temp.Insert(n);
+            }
+            while (!temp.IsEmpty())
+                q.Insert(temp.Remove());
+            return count;
+        }
+
+        private static Queue<int> DiffNums(Queue<int> q1, Queue<int>q2)
+        {
+            Queue<int> diffNums = new Queue<int>();
+            Queue<int> temp = new Queue<int>();
+            while (!q1.IsEmpty())
+            {
+                int num = q1.Remove();
+                temp.Insert(num);
+                int count1 = CountNum(q1, num);
+                int count2 = CountNum(q2, num);
+                if(count1 !=0 && count2 !=0 && count1 != count2)
+                {
+                    diffNums.Insert(num);
+                }
+            }
+            return diffNums;
+        }
+
+
+    }
+}
---
+++ studying-c-sharp-Masarwa-Shadi/DS/S3/T4/Q2.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace studying_c_sharp_Masarwa_Shadi.DS.S3.T4
+{
+    public class Q2
+    {
+        private static bool ValidLogic(Queue<char> q)
+        {
+            if (q.Head() != 'F' || q.Head() != 'T')
+                return false;
+            Queue<char> temp = new Queue<char>();
+            bool flag = true;
+            temp.Insert(q.Remove());
+            //0 - curr is logical value
+            //1 - curr is logical operator
+            int prev = 0;
+            while (!q.IsEmpty())
+            {
+                char curr = q.Remove();
+                if ((curr == 'F' || curr == 'T') && prev == 0)
+                    flag = false;
+                else
+                    prev = 1;
+
+                if ((curr == 'O' || curr == 'A') && prev == 1)
+                    flag = false;
+                else
+                    prev = 0;
+
+                temp.Insert(curr);
+            }
+            while (!q.IsEmpty()) 
+                q.Insert(q.Remove());
+            return flag && prev==0;
+        }
+    }
+}
