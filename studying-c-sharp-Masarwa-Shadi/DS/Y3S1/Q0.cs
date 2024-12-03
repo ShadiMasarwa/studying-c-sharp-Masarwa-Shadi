96edb2ba4552f5da0753998909d25178b09cd0c7
ShadiMasarwa <masarwa@gmail.com>
2024-12-03 15:46:38 +0200

    DS Q0

---
+++ studying-c-sharp-Masarwa-Shadi/DS/Y3S1/Q0.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace studying_c_sharp_Masarwa_Shadi.DS.Y3S1
+{
+    public class Q0
+    {
+        static void DeleteStackSE(Stack<int> stack, int k)
+        {
+            if (stack.IsEmpty() || stack.Top() < k)
+                return;
+            stack.Pop();
+            DeleteStackSE(stack, k);
+        }
+    }
+}
